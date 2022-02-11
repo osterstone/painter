93802a36a2cb36fc77c7656fa3511a80fe1789a8
osterstone <78485504+osterstone@users.noreply.github.com>
2022-02-11 18:06:06 +0100

    working on moving objeczt

---
+++ upaintertest/Assets/scripts/movesphere.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movesphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Pen pen = Pen.current;
+        Vector3 position = pen.position.ReadValue();
+        position.z = 88.0f;
+
+        Vector3 worldposition = Camera.main.ScreenToWorldPoint(position);
+        transform.position =  worldposition;
+    }
+}
