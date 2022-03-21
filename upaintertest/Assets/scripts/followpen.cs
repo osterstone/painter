556393c0e4eb0421383297fbc9967efd14dd2b33
osterstone <78485504+osterstone@users.noreply.github.com>
2022-03-21 13:16:47 +0100

    lockcursor confined
    Cursor wird auf das game window eingeschränkt. escape kommt da raus. beendet aber auch das spiel

--- upaintertest/Assets/scripts/followpen.cs
+++ upaintertest/Assets/scripts/followpen.cs
@@ -9,7 +9,7 @@ public class followpen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
