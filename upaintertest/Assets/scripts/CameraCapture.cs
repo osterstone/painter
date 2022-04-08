d5dbc69ffda7b80a07646afbb2181dcd1a0d7b8c
osterstone <78485504+osterstone@users.noreply.github.com>
2022-04-08 13:06:11 +0200

    screenshot new
    screenshot so gemacht, dass er die target texture zeichnet

---
+++ upaintertest/Assets/scripts/CameraCapture.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+public class CameraCapture : MonoBehaviour
+{
+    public string screenshotname = "screen";
+    private string datetime;
+    public string filePath;
+
+    private Camera Camera
+    {
+        get
+        {
+            if (!_camera)
+            {
+                _camera = Camera.main;
+            }
+            return _camera;
+        }
+    }
+    private Camera _camera;
+    void Start()
+    {
+        filePath = Application.dataPath + "/screenshots";
+        try
+        {
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Capture();
+        }
+    }
+
+    public void Capture()
+    {
+        RenderTexture activeRenderTexture = RenderTexture.active;
+        RenderTexture.active = Camera.targetTexture;
+
+        Camera.Render();
+
+        Texture2D image = new Texture2D(Camera.targetTexture.width, Camera.targetTexture.height);
+        image.ReadPixels(new Rect(0, 0, Camera.targetTexture.width, Camera.targetTexture.height), 0, 0);
+        image.Apply();
+        RenderTexture.active = activeRenderTexture;
+
+        byte[] bytes = image.EncodeToPNG();
+        Destroy(image);
+        datetime = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+        File.WriteAllBytes(Application.dataPath + "/screenshots/" + screenshotname+datetime + ".png", bytes);
+  
+    }
+}
